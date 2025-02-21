a92eb07a71f0d3ba2aed98fe453230e7c70baa36
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2025-02-20 19:43:29 -0500

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -1,6 +1,6 @@
 wget https://developer.download.nvidia.com/compute/cuda/12.2.2/local_installers/cuda_12.2.2_535.104.05_linux.runchmod +x cuda_12.8.0_570.86.10_linux.run
-sudo chown $USER:$(id -gn) /mydata
+EXPERIMENT_USER=$(ls /users | grep -v geni)
+sudo chown $EXPERIMENT_USER:$(id -gn) /mydata
 sudo mkdir -p /mydata/tmp
-sudo chown $USER:$(id -gn) /mydata/tmp
+sudo chown $EXPERIMENT_USER:$(id -gn) /mydata/tmp
 ./cuda_12.8.0_570.86.10_linux.run --silent --toolkit --toolkitpath=/mydata/toolkit --tmpdir=/mydata/tmp
-
